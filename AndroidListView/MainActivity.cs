9885814d2c764ffec20dfee6dc11f80e17b1c770
shade86 <31279365+shade86@users.noreply.github.com>
2017-11-27 21:46:30 +0100

    Initial Commit

---
+++ AndroidListView/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidListView
+{
+    [Activity(Label = "AndroidListView", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
